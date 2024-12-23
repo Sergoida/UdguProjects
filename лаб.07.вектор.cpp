a524c65ac5567b77e9e06ae5547ac589e40424dd
Sergoida <serg.ostgm@gmail.com>
2024-12-23 13:00:55 +0400

    Add files via upload

--- лаб.07.вектор.cpp
+++ лаб.07.вектор.cpp
@@ -11,21 +11,28 @@
     одну позицию к концу.
 */
 
+/*
+    Лабораторная работа №7
+    Вариант 26
+    Студент Останин С.А; группа ПИ-Э
+    Задача:
+    Дана прямоугольная целочисленная матрица размера m строк и n
+    столбцов. Последовательность элементов матрицы, расположенных
+    по чётным строкам слева направо, по нечётным строкам справа
+    налево, (строки рассматриваем последовательно сверху вниз) 
+    назовем «змейкой». Сдвинуть циклически элементы змейки на 
+    одну позицию к концу.
+*/
+
 #include <iostream>
+#include <vector>
 #include <iomanip>
 #include <locale.h>
 
 using namespace std;
 
-void newMatrix(int**& matrix, int m, int n)
-{
-matrix = new int* [m];
-for (int i = 0; i < m; i++)
-matrix[i] = new int[n];
-}
-
 // Функция для создания матрицы змейкой
-void matrixSnake(int** matrix, int m, int n) {
+void matrixSnake(vector<vector<int>>& matrix, int m, int n) {
     int num = 1;
     for (int i = 0; i < m; ++i) {
         if (i % 2 == 0) { // Четные строки
@@ -41,7 +48,7 @@ void matrixSnake(int** matrix, int m, int n) {
     }
 }
 // Функция для выполнения циклического сдвига в матрице 
-void shiftMatrix(int** matrix, int m, int n) {
+void shiftMatrix(vector<vector<int>>& matrix , int m, int n) {
     
     int a = matrix[0][0];
     int b = 1;
@@ -67,7 +74,7 @@ void shiftMatrix(int** matrix, int m, int n) {
 
 
 // Функция для вывода матрицы
-void printMatrix(int** matrix, int m, int n) {
+void printMatrix(vector<vector<int>>& matrix, int m, int n) {
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
             cout << setw(4) << matrix[i][j];
@@ -85,9 +92,8 @@ int main() {
     cout<< "Введите количество столбцов"<< endl;
     cin>> n;
     
-    int** matrix; 
+    vector<vector<int>> matrix(m, vector<int>(n)); 
 
-    newMatrix(matrix, m, n);
     matrixSnake(matrix, m, n);
     cout << "Матрица:" << endl;
     printMatrix(matrix, m, n);
