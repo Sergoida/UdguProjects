926ce3d6899d752f4bd54eb256d181241002935b
Sergoida <serg.ostgm@gmail.com>
2025-01-09 19:18:18 +0400

    Add files via upload

--- лаб.07.вектор.cpp
+++ лаб.07.вектор.cpp
@@ -25,14 +25,14 @@ void newMatrix(vector<vector<int>>& matrix, int m, int n) {
 // Функция для создания матрицы змейкой
 void matrixSnake(vector<vector<int>>& matrix) {
     int num = 1;
-    for (size_t i = 0; i < matrix.size(); ++i) {
-        if (i % 2 == 0) { // Четные строки
-            for (size_t j = 0; j < matrix[i].size(); ++j) {
-                matrix[i][j] = num++;
+    for (auto row = matrix.begin(); row != matrix.end(); ++row) {
+        if ((row - matrix.begin()) % 2 == 0) { // Четные строки
+            for (auto col = row->begin(); col != row->end(); ++col) {
+                *col = num++;
             }
         } else { // Нечетные строки
-            for (size_t j = matrix[i].size(); j > 0; --j) {
-                matrix[i][j - 1] = num++;
+            for (auto col = row->end() - 1; col >= row->begin(); --col) {
+                *col = num++;
             }
         }
     }
@@ -40,22 +40,22 @@ void matrixSnake(vector<vector<int>>& matrix) {
 
 // Функция для выполнения циклического сдвига в матрице
 void shiftMatrix(vector<vector<int>>& matrix) {
-    int a = matrix[0][0];
+    int a = 1;
     int b = 1;
-    for (size_t i = 0; i < matrix.size(); ++i) {
-        if (i % 2 == 0) { // Четные строки
-            for (size_t j = 0; j < matrix[i].size(); ++j) {
-                if (i == 0 && j == 0) continue; // Пропускаем первую ячейку
-                b = matrix[i][j];
-                matrix[i][j] = a; 
+    for (auto row = matrix.begin(); row != matrix.end(); ++row) {
+        if ((row - matrix.begin()) % 2 == 0) { // Четные строки
+            for (auto col = row->begin(); col != row->end(); ++col) {
+                if (row == matrix.begin() && col == row->begin()) continue; // Пропускаем первую ячейку
+                b = *col;
+                *col = a;
                 a = b;
             }
         } else { // Нечетные строки
-            for (size_t j = matrix[i].size(); j > 0; --j) {
-                if (i == matrix.size() - 1 && j == matrix[i].size()) continue; // Пропускаем последнюю ячейку
-                b = matrix[i][j - 1];
-                matrix[i][j - 1] = a; 
-                a = b; 
+            for (auto col = row->end() - 1; col >= row->begin(); --col) {
+                if (row == matrix.end() - 1 && col == row->end() - 1) continue; // Пропускаем последнюю ячейку
+                b = *(col);
+                *col = a;
+                a = b;
             }
         }
     }
@@ -68,7 +68,7 @@ void printMatrix(const vector<vector<int>>& matrix) {
         for (const auto& element : row) {
             cout << setw(4) << element;
         }
-        cout << endl; 
+        cout << endl;
     }
 }
 
@@ -76,22 +76,21 @@ int main() {
     setlocale(LC_ALL, "Russian");
     int m;
     int n;
-    cout<< "Введите количество строк"<< endl;
-    cin>> m;
-    cout<< "Введите количество столбцов"<< endl;
-    cin>> n;
-    
-    vector<vector<int>> matrix(m, vector<int>(n));
+    cout << "Введите количество строк" << endl;
+    cin >> m;
+    cout << "Введите количество столбцов" << endl;
+    cin >> n;
 
+    vector<vector<int>> matrix(m, vector<int>(n));
 
     newMatrix(matrix, m, n);
     matrixSnake(matrix);
     cout << "Матрица:" << endl;
     printMatrix(matrix);
-    
+
     shiftMatrix(matrix);
     cout << "Матрица после циклического сдвига:" << endl;
     printMatrix(matrix);
-    
+
     return 0;
 }
