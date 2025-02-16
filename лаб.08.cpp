73d5e7add50372c8b3b69014148ea511ae6d2bdb
Sergoida <serg.ostgm@gmail.com>
2025-02-16 23:23:18 +0400

    Update лаб.08.cpp

--- лаб.08.cpp
+++ лаб.08.cpp
@@ -9,23 +9,25 @@
 
 #include <iostream>
 #include <fstream>
-#include <string>
-
+#include <cstring>
 using namespace std;
 
-bool SeekString(const string &s1, const string &s2 ,int L) {
+// Основная функция
+bool SeekString(const string& s1, const string& s2, int L, string& result) {
     if (L <= 0 || s1.length() < L || s2.length() < L) {
         return false;
     }
-    
+
     // Проверяем все подстроки длиной L в первой строке
     for (size_t i = 0; i <= s1.length() - L; ++i) {
         string substring = s1.substr(i, L);
-        
+
         // Сравниваем подстроку из первой строки с подстроками во второй строке
         for (size_t j = 0; j <= s2.length() - L; ++j) {
             if (substring == s2.substr(j, L)) {
+                result = substring; // Сохраняем найденную подстроку
                 return true; // Найдена общая подстрока
+            }
         }
     }
 
@@ -34,26 +36,22 @@ bool SeekString(const string &s1, const string &s2 ,int L) {
 
 int main() {
     setlocale(LC_ALL, "Russian");
-    ifstream inputFile("input.txt");
-    
-    if (!inputFile.is_open()) {
-        cerr << "Файл невозможно открыть"  << endl;
-        exit(1);
-    }
 
     string s1, s2;
+    string result;
     int L;
 
-    // Читаем строки и длину подстроки из файла
-    cout << "Введите 1 строку " << endl;
-    inputFile >> s1 >> L;
-    cout << "Введите 2 строку " << endl;
-    inputFile >> s2 >> L;
-    inputFile.close();
+    // Читаем строки и длину подстроки из консоли
+    cout << "Введите 1 строку: " << endl;
+    getline(cin, s1);
+    cout << "Введите 2 строку: " << endl;
+    getline(cin, s2);
+    cout << "Введите длину подстроки: " << endl;
+    cin >> L;
 
-    if (SeekString(s1, s2, L)) {
+    if (SeekString(s1, s2, L, result)) {
         cout << "Есть общая подстрока длиной " << L << "." << endl;
-        cout << "Подстрока: " << lstring << "." << endl;
+        cout << "Подстрока: " << result << "." << endl;
     } else {
         cout << "Нет общей подстроки длиной " << L << "." << endl;
     }
